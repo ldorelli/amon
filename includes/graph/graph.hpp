d990bad9a9fa1fecbf7cb4a390b99c70a8a9863b
fushigishi <lfdorelli@gmail.com>
2015-08-13 23:31:25 -0300

    bla

--- includes/graph/graph.hpp
+++ includes/graph/graph.hpp
@@ -1,5 +1,8 @@
 #include <vector>
 #include <set>
+#include <queue>
+#include <utility>
+#include <list>
 
 namespace amon {
 class Graph {
@@ -23,13 +26,13 @@ public:
 	 * Returns the number of nodes in the graph.
 	 * @return The number of nodes in the graph.
 	 */
-	int nodesQty();
+	int nodesQty() const;
 
 	/**
 	 *	Returns the number of edges in the graph.
 	 * 	@return The number of edges in the graph.
 	 */
-	int edgesQty();
+	int edgesQty() const;
 
 	/**
 	 * Adds a node to the graph.
@@ -45,12 +48,61 @@ public:
 	 void removeNode(int index);
 
 
+	/**
+	 * Adds a directed edge between vertices A and B with weight w.
+	 * @param A The source of the edge
+	 * @param B The destination of the edge
+	 * @param w The weight of the edge
+	 */
+	void addDirectedEdge (int A, int B, double w);
+
+	/**
+	 *	Adds an undirected edge between vertices A and B with weight w.
+	 *	@param A One of the end-points of the edge
+	 * 	@param B The other endpoint
+	 * @param w The weight of the edge
+	 */
+	void addUndirectedEdge (int A, int B, double w);
+
+
+	/**
+	 *	Calculates the mean degree of the graph
+	 *  @return The mean degree of the graph
+	 */
+	double meanDegree ();
+
+
+	/**
+	 * Returns an iterator to the begining of the adjacency list of 
+	 * a node. 
+	 * @param node The node to get neighboors from
+	 * @return An iterator to the beggining of the node list.
+	 */
+	std::list<std::pair<int, double> >::iterator 
+		neighboorsBegin(int node);
+
+	/**
+	 * Returns a const_iterator to the end of the adjacency list of node.
+	 * @param node The node to get the iterator from	 
+	 */
+	std::list<std::pair<int, double> >::iterator 
+		neighboorsEnd(int node);
+
+
+	/**
+	 * Advances the adjacency list iterator to the next neighboor. 
+	 * @param iterator The iterator to advance
+	 * @param node The node related to the iterator 
+	 */
+	void nextNeighboor (std::list<std::pair<int, double> >::iterator 
+		& iterator, int node);
 
 private:
+	int nodesCount, edgesCount;
 	// Adjacency list and weights
-	vector< vector <int> > adj;
-	vector < vector <double> > weights;
+	std::vector< std::list <std::pair<int, double> > > adj;
 	// validNotes[i] is true if node i is active
-	vector <bool> validNodes;
+	std::vector <bool> validNodes;
+	std::queue <int> availableIndexes;
 };
 }
--- src/graph/graph.cpp
+++ src/graph/graph.cpp
@@ -1,1 +1,96 @@
 #include <graph.hpp>
+
+amon::Graph::Graph() {
+}
+
+amon::Graph::~Graph() {
+	adj.clear();
+	validNodes.clear();
+	while (availableIndexes.size()) availableIndexes.pop();
+}
+
+
+int amon::Graph::nodesQty() const {
+	return nodesCount;
+}
+
+int amon::Graph::edgesQty() const {
+	return edgesCount;	
+}
+
+int amon::Graph::addNode () {
+	int idx;
+	if (availableIndexes.size()) {
+		idx = availableIndexes.front();
+		availableIndexes.pop();
+		validNodes[idx] = true;
+		adj[idx].clear();
+	} else {
+		idx = adj.size();
+		adj.push_back(std::list<std::pair<int, double> >());
+	}
+	nodesCount++;
+	return idx;
+}
+
+
+void amon::Graph::removeNode (int index) {
+	if (index > (int) adj.size() or index < 0) return;
+	adj[index].clear();
+	validNodes[index] = false;
+}
+
+void amon::Graph::addDirectedEdge (int a, int b, double w) {
+	if (a < 0 or a > nodesCount or !validNodes[a]) return;
+	if (b < 0 or b > nodesCount or !validNodes[b]) return;
+	adj[a].push_back(std::make_pair(b, w));
+	this->edgesCount++;
+}
+
+void amon::Graph::addUndirectedEdge(int a, int b, double w) {
+	if (a < 0 or a > nodesCount or !validNodes[a]) return;
+	if (b < 0 or b > nodesCount or !validNodes[b]) return;
+	adj[a].push_back( std::make_pair (b, w));
+	adj[b].push_back( std::make_pair(a, w));
+	this->edgesCount++;
+}
+
+std::list<std::pair<int, double> >::iterator
+	amon::Graph::neighboorsBegin (int node) {
+	
+	if (node < 0 or node > nodesCount or !validNodes[node]) {
+		throw "Invalid node index";
+	}
+	auto it = adj[node].begin();
+	while (it != adj[node].end() and !validNodes[it->first]) {
+		it = adj[node].erase(it);
+	}
+	return it;
+}
+
+
+std::list<std::pair<int, double> >::iterator
+	amon::Graph::neighboorsEnd (int node) {
+	
+	if (node < 0 or node > nodesCount or !validNodes[node]) {
+		throw "Invalid node index";
+	}
+	return adj[node].end();
+}
+
+void amon::Graph::nextNeighboor (
+	std::list<std::pair<int, double> >::iterator &it, int node) {
+	
+	if (it == adj[node].end()) return;	
+	
+	int edge = it->first;
+	if (!validNodes[edge]) {
+		it = adj[node].erase(it);
+	} else {
+		it++;
+	}
+}
+
+double amon::Graph::meanDegree () {
+	return (double) edgesCount/nodesCount;
+}
